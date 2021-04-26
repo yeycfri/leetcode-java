aee34b7fb6b81c6d05e6b150dff57650f4b3fde3
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-04-26 19:12:58 +0800

    leetcode 1

---
+++ src/_0001_Two_Sum/Solution.java
@@ -0,0 +1,18 @@
+package _0001_Two_Sum;
+
+import java.util.HashMap;
+
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int sub;
+        for (int i = 0; i < nums.length; i++) {
+            sub = target - nums[i];
+            if (map.containsKey(nums[i])) {
+                return new int[]{map.get(nums[i]), i};
+            }
+            map.put(sub, i);
+        }
+        return null;
+    }
+}
