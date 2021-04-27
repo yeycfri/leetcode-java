8c04a5b2ab2738f63616e07a717656e450bc9e30
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-04-27 14:07:43 +0800

    leetcode 283

---
+++ src/_0283_Move_Zeroes/Solution.java
@@ -0,0 +1,20 @@
+package _0283_Move_Zeroes;
+
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0, right = 0;
+        while (right < nums.length) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int tmp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = tmp;
+    }
+}
