f4341376476baf86c27946085438dfca9ea9325e
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-05-23 10:29:20 +0800

    leetcode 704 Binary Search

---
+++ src/_0704_Binary_Search/Solution.java
@@ -0,0 +1,19 @@
+package _0704_Binary_Search;
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (target < nums[0] || target > nums[nums.length - 1]) return -1;
+        int left = 0, right = nums.length;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return mid;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return -1;
+    }
+}
