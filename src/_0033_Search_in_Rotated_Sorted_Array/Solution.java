c0ffd2c4028bb242d5181addfc99b474ca771796
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-05-26 10:27:12 +0800

    leetcode 33

---
+++ src/_0033_Search_in_Rotated_Sorted_Array/Solution.java
@@ -0,0 +1,32 @@
+package _0033_Search_in_Rotated_Sorted_Array;
+
+public class Solution {
+    public int search(int[] nums, int target) {
+        if (nums.length == 0) return -1;
+        if (nums.length == 1) return nums[0] == target ? 0 : -1;
+
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target <= nums[mid]) {
+                    right = mid;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+        }
+        return nums[left] == target ? left : -1;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
+    }
+}
