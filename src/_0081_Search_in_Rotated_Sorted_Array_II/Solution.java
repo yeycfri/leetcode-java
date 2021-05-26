f92271be8a47bc54ac4eb43b6000d2e5f6cfd88b
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-05-26 11:01:23 +0800

    leetcode 81

---
+++ src/_0081_Search_in_Rotated_Sorted_Array_II/Solution.java
@@ -0,0 +1,33 @@
+package _0081_Search_in_Rotated_Sorted_Array_II;
+
+public class Solution {
+    public boolean search(int[] nums, int target) {
+        if (nums.length == 0) return false;
+        if (nums.length == 1) return nums[0] == target;
+
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + ((right - left) >> 1);
+            if (nums[mid] == target) {
+                return true;
+            }
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
+                left++;
+                right--;
+            } else if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
+        }
+        return false;
+    }
+}
