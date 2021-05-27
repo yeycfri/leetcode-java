88a74cd427ca88a0c3e6313310bf9c014051f0cb
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-05-27 21:35:18 +0800

    leetcode 852

---
+++ src/_0852_Peak_Index_in_a_Mountain_Array/Solution.java
@@ -0,0 +1,16 @@
+package _0852_Peak_Index_in_a_Mountain_Array;
+
+public class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            int mid = left + ((right - left) >> 1);
+            if (arr[mid] < arr[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
