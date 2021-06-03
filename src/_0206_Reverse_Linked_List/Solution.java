362f29deabdf3ed28a1608d908d234ca6e3d7363
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-03 14:46:35 +0800

    leetcode 206

---
+++ src/_0206_Reverse_Linked_List/Solution.java
@@ -0,0 +1,31 @@
+package _0206_Reverse_Linked_List;
+
+public class Solution {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
