7b228dc800a0e78731c8bbfe664db8b062d176fb
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-15 19:05:46 +0800

    leetcode 20

---
+++ src/_0020_Valid_Parentheses/Solution.java
@@ -0,0 +1,33 @@
+package _0020_Valid_Parentheses;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Stack;
+
+public class Solution {
+    public boolean isValid(String s) {
+        int n = s.length();
+        if (n % 2 != 0) {
+            return false;
+        }
+
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == ')') {
+                if (stack.isEmpty() || stack.pop() != '(') return false;
+            } else if (c == ']') {
+                if (stack.isEmpty() || stack.pop() != '[') return false;
+            } else if (c == '}') {
+                if (stack.isEmpty() || stack.pop() != '{') return false;
+            } else {
+                stack.push(c);
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.isValid("()"));
+    }
+}
