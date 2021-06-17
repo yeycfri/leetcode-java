668d29aeb27a6ddb3c4532837a64c7ce61b77ac8
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-17 15:10:21 +0800

    leetcode 94 144 145 preorder inorder postorder traversal iterate methods

--- src/_0144_Binary_Tree_Preorder_Traversal/Solution.java
+++ src/_0144_Binary_Tree_Preorder_Traversal/Solution.java
@@ -1,9 +1,12 @@
 package _0144_Binary_Tree_Preorder_Traversal;
 
 import common.bst.TreeNode;
+import sun.reflect.generics.tree.Tree;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
@@ -17,5 +20,39 @@ public class Solution {
         ans.add(root.val);
         traversal(ans, root.left);
         traversal(ans, root.right);
+    }
+
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            ans.add(root.val);
+            if (root.right != null) stack.push(root.right);
+            if (root.left != null) stack.push(root.left);
+        }
+        return ans;
+    }
+
+    // 迭代法统一模板，插入null标记
+    public List<Integer> preorderTraversal2(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.peek();
+            if (root != null) {
+                stack.pop();
+                if (root.right != null) stack.push(root.right);
+                if (root.left != null) stack.push(root.left);
+                stack.push(root);
+                stack.push(null);
+            } else {
+                stack.pop();
+                ans.add(stack.pop().val);
+            }
+        }
+        return ans;
     }
 }
--- src/_0145_Binary_Tree_Postorder_Traversal/Solution.java
+++ src/_0145_Binary_Tree_Postorder_Traversal/Solution.java
@@ -1,9 +1,9 @@
 package _0145_Binary_Tree_Postorder_Traversal;
 
 import common.bst.TreeNode;
+import sun.reflect.generics.tree.Tree;
 
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 
 public class Solution {
     public List<Integer> postorderTraversal(TreeNode root) {
@@ -17,5 +17,40 @@ public class Solution {
         traversal(ans, root.left);
         traversal(ans, root.right);
         ans.add(root.val);
+    }
+
+    public List<Integer> postorderTraversal1(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.pop();
+            ans.add(root.val);
+            if (root.left != null) stack.push(root.left);
+            if (root.right != null) stack.push(root.right);
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+
+    // 迭代法统一模板，插入null标记
+    public List<Integer> postorderTraversal2(TreeNode root) {
+        List<Integer> ans = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack<>();
+        if (root != null) stack.push(root);
+        while (!stack.isEmpty()) {
+            root = stack.peek();
+            if (root != null) {
+                stack.pop();
+                stack.push(root);
+                stack.push(null);
+                if (root.right != null) stack.push(root.right);
+                if (root.left != null) stack.push(root.left);
+            } else {
+                stack.pop();
+                ans.add(stack.pop().val);
+            }
+        }
+        return ans;
     }
 }
