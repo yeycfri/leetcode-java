8cc161901e9d7308409379d582591099db7f0088
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-19 22:11:58 +0800

    leetcode 404

---
+++ src/_0404_Sum_of_Left_Leaves/Solution.java
@@ -0,0 +1,33 @@
+package _0404_Sum_of_Left_Leaves;
+
+import common.bst.TreeNode;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+    public int sumOfLeftLeaves(TreeNode root) {
+        if (root == null) return 0;
+        int sum = 0;
+        if (root.left != null && root.left.left == null & root.left.right == null) {
+            sum = root.left.val;
+        }
+        return sum + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+    }
+
+    public int sumOfLeftLeavesBFS(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        if (root != null) queue.offer(root);
+        int sum = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                root = queue.poll();
+                if (root.left != null && root.left.left == null && root.left.right == null) sum += root.left.val;
+                if (root.left != null) queue.offer(root.left);
+                if (root.right != null) queue.offer(root.right);
+            }
+        }
+        return sum;
+    }
+}
