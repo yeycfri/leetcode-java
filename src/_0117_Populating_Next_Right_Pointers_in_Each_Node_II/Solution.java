82a038cfd8adad225d138f53077411e8efec5169
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-19 14:24:52 +0800

    leetcode 117

---
+++ src/_0117_Populating_Next_Right_Pointers_in_Each_Node_II/Solution.java
@@ -0,0 +1,45 @@
+package _0117_Populating_Next_Right_Pointers_in_Each_Node_II;
+
+public class Solution {
+    class Node {
+        public int val;
+        public Node left;
+        public Node right;
+        public Node next;
+
+        public Node() {
+        }
+
+        public Node(int _val) {
+            val = _val;
+        }
+
+        public Node(int _val, Node _left, Node _right, Node _next) {
+            val = _val;
+            left = _left;
+            right = _right;
+            next = _next;
+        }
+    }
+
+    public Node connect(Node root) {
+        Node cur = root;
+        while (cur != null) {
+            Node dummy = new Node(0);
+            Node prev = dummy;
+            while (cur != null) {
+                if (cur.left != null) {
+                    prev.next = cur.left;
+                    prev = prev.next;
+                }
+                if (cur.right != null) {
+                    prev.next = cur.right;
+                    prev = prev.next;
+                }
+                cur = cur.next;
+            }
+            cur = dummy.next;
+        }
+        return root;
+    }
+}
