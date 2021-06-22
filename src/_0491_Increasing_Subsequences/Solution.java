f7f1affcf9b47ce3a179664d98cd3043efc594a0
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-23 00:57:35 +0800

    leetcode 491

---
+++ src/_0491_Increasing_Subsequences/Solution.java
@@ -0,0 +1,37 @@
+package _0491_Increasing_Subsequences;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> path = new ArrayList<>();
+
+    public List<List<Integer>> findSubsequences(int[] nums) {
+        backtracking(nums, 0);
+        return ans;
+    }
+
+    private void backtracking(int[] nums, int idx) {
+        if (path.size() > 1) ans.add(new ArrayList<>(path));
+        if (idx == nums.length) return;
+
+        int[] set = new int[201];
+        for (int i = idx; i < nums.length; i++) {
+            if ((!path.isEmpty() && nums[i] < path.get(path.size() - 1)) || set[nums[i] + 100] == 1) {
+                continue;
+            }
+            set[nums[i] + 100] = 1;
+            path.add(nums[i]);
+            backtracking(nums, i + 1);
+            path.remove(path.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findSubsequences(new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 1, 1, 1, 1}));
+    }
+}
