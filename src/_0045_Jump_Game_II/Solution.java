c0adbe50117940022161e2708d3e6faffa70bbdd
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-25 20:27:55 +0800

    leetcode 45

---
+++ src/_0045_Jump_Game_II/Solution.java
@@ -0,0 +1,15 @@
+package _0045_Jump_Game_II;
+
+public class Solution {
+    public int jump(int[] nums) {
+        int ans = 0, curCover = 0, nextCover = 0;
+        for (int i = 0; i < (nums.length - 1); i++) {
+            nextCover = Math.max(nextCover, i + nums[i]);
+            if (i == curCover) {
+                ans++;
+                curCover = nextCover;
+            }
+        }
+        return ans;
+    }
+}
