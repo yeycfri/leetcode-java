26fa828d3e51fcd215213b669d550389178084c9
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-06-29 14:34:09 +0800

    leetcode 435

---
+++ src/_0435_Non_overlapping_Intervals/Solution.java
@@ -0,0 +1,20 @@
+package _0435_Non_overlapping_Intervals;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int eraseOverlapIntervals(int[][] intervals) {
+        Arrays.sort(intervals, (o1, o2) -> Integer.compare(o1[0], o2[0]));
+        int ans = 1;
+        int right = intervals[0][1];
+        for (int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] >= right) {
+                ans++;
+                right = intervals[i][1];
+            } else {
+                right = Math.min(right, intervals[i][1]);
+            }
+        }
+        return intervals.length - ans;
+    }
+}
