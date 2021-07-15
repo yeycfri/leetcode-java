ff372adb85a44c735def0c36a461d50f969735d8
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-07-15 10:42:03 +0800

    leetcode 718

---
+++ src/DynamicProgramming/_0718_Maximum_Length_of_Repeated_Subarray/Solution.java
@@ -0,0 +1,20 @@
+package DynamicProgramming._0718_Maximum_Length_of_Repeated_Subarray;
+
+public class Solution {
+    public int findLength(int[] nums1, int[] nums2) {
+        int m = nums1.length, n = nums2.length;
+        int[] dp = new int[n + 1];
+        int ans = 0;
+        for (int i = 1; i <= m; i++) {
+            for (int j = n; j >= 1; j--) {
+                if (nums1[i - 1] == nums2[j - 1]) {
+                    dp[j] = dp[j - 1] + 1;
+                } else {
+                    dp[j] = 0;
+                }
+                ans = Math.max(ans, dp[j]);
+            }
+        }
+        return ans;
+    }
+}
