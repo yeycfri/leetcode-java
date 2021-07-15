32a9abc6d36bbabaf3791fc1c76028b52f35f019
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-07-15 17:02:05 +0800

    leetcode 583

--- src/DynamicProgramming/_1143_Longest_Common_Subsequence/Solution.java
+++ src/DynamicProgramming/_1143_Longest_Common_Subsequence/Solution.java
@@ -4,7 +4,6 @@ public class Solution {
     public int longestCommonSubsequence(String text1, String text2) {
         int m = text1.length(), n = text2.length();
         int[][] dp = new int[m + 1][n + 1];
-        int ans = 0;
         for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= n; j++) {
                 if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
@@ -12,9 +11,8 @@ public class Solution {
                 } else {
                     dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
                 }
-                ans = Math.max(ans, dp[i][j]);
             }
         }
-        return ans;
+        return dp[m][n];
     }
 }
