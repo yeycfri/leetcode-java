11c3857a4521037e4e9ae9f40214896ce20eac78
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-08-05 15:48:16 +0800

    leetcode 27

--- src/TwoPointers/_0027_Remove_Element/Solution.java
+++ src/TwoPointers/_0027_Remove_Element/Solution.java
@@ -2,12 +2,12 @@ package TwoPointers._0027_Remove_Element;
 
 public class Solution {
     public int removeElement(int[] nums, int val) {
-        int slow = 0;
-        for (int fast = 0; fast < nums.length; fast++) {
-            if (nums[fast] != val) {
-                nums[slow++] = nums[fast];
-            }
+        int l = 0, r = 0, n = nums.length;
+        while (r < n) {
+            if (nums[r] != val)
+                nums[l++] = nums[r];
+            r++;
         }
-        return slow;
+        return l;
     }
 }
