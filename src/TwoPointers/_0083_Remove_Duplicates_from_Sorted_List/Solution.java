2a9923e7c0c701b12c489fda7f039ffc97935e44
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-08-05 16:22:30 +0800

    leetcode 83

---
+++ src/TwoPointers/_0083_Remove_Duplicates_from_Sorted_List/Solution.java
@@ -0,0 +1,17 @@
+package TwoPointers._0083_Remove_Duplicates_from_Sorted_List;
+
+import common.ListNode;
+
+public class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode cur = head;
+        while (cur != null && cur.next != null) {
+            if (cur.next.val == cur.val) {
+                cur.next = cur.next.next;
+            } else {
+                cur = cur.next;
+            }
+        }
+        return head;
+    }
+}
