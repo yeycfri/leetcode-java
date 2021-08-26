06a47f734775dc997462f1bd73ff9c2f6acc12dd
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-08-26 14:28:13 +0800

    leetcode 39

--- src/Backtracking/_0039_Combination_Sum/Solution.java
+++ src/Backtracking/_0039_Combination_Sum/Solution.java
@@ -1,33 +1,30 @@
 package Backtracking._0039_Combination_Sum;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class Solution {
 
-    List<List<Integer>> ans = new ArrayList<>();
-    List<Integer> arr = new ArrayList<>();
-    int sum = 0;
+    List<List<Integer>> ans;
+    List<Integer> path;
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        Arrays.sort(candidates);
-        backtracking(candidates, 0, target);
+        ans = new ArrayList<>();
+        path = new ArrayList<>();
+        dfs(candidates, 0, target);
         return ans;
     }
 
-    private void backtracking(int[] candidates, int start, int target) {
-        if (sum > target) return;
-        if (sum == target) {
-            ans.add(new ArrayList<>(arr));
+    private void dfs(int[] cand, int idx, int target) {
+        if (target < 0) return;
+        if (target == 0) {
+            ans.add(new ArrayList<>(path));
             return;
         }
-        for (int i = start; i < candidates.length && (sum + candidates[i] > target); i++) {
-            arr.add(candidates[i]);
-            sum += candidates[i];
-            backtracking(candidates, i, target);
-            arr.remove(arr.size() - 1);
-            sum -= candidates[i];
+        for (int i = idx; i < cand.length; i++) {
+            path.add(cand[i]);
+            dfs(cand, i, target - cand[i]);
+            path.remove(path.size() - 1);
         }
     }
 }
