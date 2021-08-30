24f0eb556ac7e1b2d98ddc45a5fd650090a0a5c7
Yaocheng Ye <yeyc.sailing@outlook.com>
2021-08-30 12:40:22 +0800

    leetcode 93

--- src/Backtracking/_0093_Restore_IP_Addresses/Solution.java
+++ src/Backtracking/_0093_Restore_IP_Addresses/Solution.java
@@ -4,42 +4,38 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Solution {
-    List<String> ans = new ArrayList<>();
-    int[] seg = new int[4];
+    List<String> ans;
+    int[] seg;
 
     public List<String> restoreIpAddresses(String s) {
-        if (s.length() < 4 || s.length() > 12) return ans;
+        ans = new ArrayList<>();
+        seg = new int[4];
         backtracking(s, 0, 0);
         return ans;
     }
 
-    private void backtracking(String s, int splitIdx, int segId) {
+    private void backtracking(String s, int idx, int segId) {
+        int n = s.length();
         if (segId == 4) {
-            if (splitIdx == s.length()) {
-                StringBuilder str = new StringBuilder(s.length() + 3);
-                str.append(seg[0]);
-                for (int i = 1; i < 4; i++) {
-                    str.append('.').append(seg[i]);
-                }
-                ans.add(str.toString());
+            if (idx == n) {
+                StringBuilder sb = new StringBuilder(n + 3);
+                sb.append(seg[0]).append('.').append(seg[1]).append('.').append(seg[2]).append('.').append(seg[3]);
+                ans.add(sb.toString());
             }
             return;
         }
-        if (splitIdx == s.length()) return;
-        if (s.charAt(splitIdx) == '0') {
+        if (idx == n) return;
+        if (s.charAt(idx) == '0') {
             seg[segId] = 0;
-            backtracking(s, splitIdx + 1, segId + 1);
+            backtracking(s, idx + 1, segId + 1);
             return;
         }
         int num = 0;
-        for (int i = splitIdx; i < s.length(); i++) {
-            num = num * 10 + (s.charAt(i) - '0');
-            if (num > 0 && num <= 255) {
-                seg[segId] = num;
-                backtracking(s, i + 1, segId + 1);
-            } else {
-                break;
-            }
+        for (int i = idx; i < n; i++) {
+            num = num * 10 + s.charAt(i) - '0';
+            if (num > 255) break;
+            seg[segId] = num;
+            backtracking(s, i + 1, segId + 1);
         }
     }
 
